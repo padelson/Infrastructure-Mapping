d84f1a5099135064693534b9380ac969a7d6f659
Barak Oshri <barakoshri@gmail.com>
2017-10-17 11:34:25 -0700

    small changes logistic regression model

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -1,15 +1,16 @@
 from keras.models import Sequential 
 from keras.layers import Dense, Activation 
 
-def get_logreg_model(input_dim):
+def logreg_model(input_dim):
 	output_dim = 2 # Binary
 	model = Sequential() 
 	model.add(Dense(output_dim, input_dim=input_dim, activation='softmax')) 
 	model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy']) 
 	return model
 
+# Expects: logreg_model, [num_examples, length of band], [num_examples, 1], batch size, number epochs
 # Returns [Training Score, Training Accuracy], [Test score, Test Accuracy]
-def train_model(model, X, Y, batch_size, num_epoch, validation_split = 0.8):
+def train_model(model, X, Y, batch_size, num_epoch):
 	X_train, X_test = X[:0.8*len(X)], X[0.8*len(X):]
 	Y_train, Y_test = Y[:0.8(len(Y)), Y[0.8*len(Y):]
 
