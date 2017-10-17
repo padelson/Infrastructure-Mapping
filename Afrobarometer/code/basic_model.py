86bed068fc8bd3ed41277bfd2c4bd0cb6201a1ec
padelson <padelson@stanford.edu>
2017-10-17 18:25:26 +0000

    creates features from saved sat np arrays and target feature

---
+++ Afrobarometer/code/basic_model.py
@@ -0,0 +1,26 @@
+import csv
+import numpy as np
+import gdal
+import os
+
+
+with open("/mnt/mounted_bucket/Afrobarometer_R6.csv", 'r') as f:
+    survey = list(csv.reader(f, delimiter= ","))
+
+ids = []
+X = []
+
+for filename in os.listdir('saved_images'):
+    id = int(filename[4:-4])
+    arr = np.load("saved_images/"+filename)
+    #print (np.mean(arr, axis = 0))
+    X.append(np.mean(np.mean(arr, axis = 0), axis = 0))
+    ids.append(id)
+
+X = np.array(X)
+Y = []
+for i in range(0, len(ids)):
+    id = ids[i]
+    item = survey[id]
+    Y.append(int(item[19])) #19 corresponds to electricity
+Y = np.array(Y)
