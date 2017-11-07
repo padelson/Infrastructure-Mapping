70b70cafdeabfa8b0163a394fbc423f4a917ea3c
Annie <anniehu4@gmail.com>
2017-11-06 23:42:18 -0800

    adding continuous variables

--- addis_histogram_baseline_knn.py
+++ addis_histogram_baseline_knn.py
@@ -18,24 +18,24 @@ verbose = True
 nbins = 100
 nbands = 4
 data = pd.read_csv('Addis_data_processed.csv')
-satellite = 's1'
-path = '/mnt/mounted_bucket/saved_img/' + satellite
 
 def predict_all(names):
 	all_features = data[['lat', 'long']]
+	indices = list(range(len(data)))
+	random.shuffle(indices)
 	split = int(len(data) * 0.8)
-	x_train, x_test = all_features[:split], all_features[split:]
+	x_train, x_test = all_features.ix[indices[:split]], all_features.ix[indices[split:]]
 	print "loaded %d training, %d testing samples" % (len(x_train), len(x_test))
 	all_results = {}
 	for col in names:
 		all_vals = data[col]
-		y_train = all_vals[:split]
-		y_test = all_vals[split:]
+		y_train = all_vals.ix[indices[:split]]
+		y_test = all_vals.ix[indices[split:]]
 		
 		if np.max(y_train) == 0:
 			print "all 0 column!"
 			continue
-		print "Training model"
+		print "Training model",col
 		reg = KNeighborsClassifier()
 		reg.fit(x_train, y_train)
 		predictions = reg.predict(x_test)
@@ -43,11 +43,14 @@ def predict_all(names):
 		precision, recall, f1, support = precision_recall_fscore_support(y_train, train_predict, average='binary')
 		print "Training\tF1:%f, Precision:%f, Recall:%f" % (f1, precision,recall)
 		precision, recall, f1, support = precision_recall_fscore_support(y_test, predictions, average='binary')
-		print "Testing\tF1:%f, Precision:%f, Recall:%f" % (f1, precision,recall)
-	joblib.dump(all_results, satellite + '_histogram_results_' + predictor + '.pkl', compress=True)
+		if f1 > 0:
+			print "Testing\tF1:%f, Precision:%f, Recall:%f" % (f1, precision,recall)
+		all_results[col] = f1
+	joblib.dump(all_results, 'histogram_results_knn.pkl', compress=True)
 	# joblib.dump(all_results, 'logreg_test.pkl')
 
 def main():
 	predict_all(addis.binary_features)
+
 if __name__ == '__main__':
 	main()
