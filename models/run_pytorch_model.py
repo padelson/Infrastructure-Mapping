f01b6092893f0c0de09113acce0acd3df767aad7
BarakOshri <barakoshri@gmail.com>
2017-11-13 22:00:07 +0000

    Changes

--- models/run_pytorch_model.py
+++ models/run_pytorch_model.py
@@ -1,6 +1,5 @@
 # from __future__ import print_function, division
-import sys
-sys.path.append("..")
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -14,35 +13,28 @@ import matplotlib.pyplot as plt
 import time
 import os
 import pandas as pd
-from utils import addis as util
 from sklearn.metrics import f1_score
 
-satellite = 'l8'
-filetail = ".0.npy"
-continuous = False 
-lr = 1e-4
-momentum = 0.9
 len_dataset = 3591
-
 data_dir = '../addis_s1_center_cropped'
 column = 'pit_latrine_depth_val2_when_bl_dw39_val1'
 
-num_examples = 100
+num_examples = 3591
 train_test_split = 0.9
 continuous = False
-lr = 1e-4 # was 0.01 for binary
-momentum = 0.5 # was 0.4 for binary
+lr = 1e-3 # was 0.01 for binary
+momentum = 0.3 # was 0.4 for binary
 last_many_f1 = 5
-batch_size = 10
+batch_size = 64
 num_workers = 4
 num_epochs = 20
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
-    # all_results = open(satellite + '_results.csv', 'w')
+
     best_model_wts = model.state_dict()
     best_acc = 0.0
-    best_train_acc = 0.0
+
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
@@ -106,17 +98,15 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             epoch_acc = running_corrects / len(current_dataset)
             if not continuous and epoch >= num_epochs - last_many_f1:
                 epoch_f1 = f1_score(current_dataset.data, running_preds)
-		print('{} Loss: {:.4f} Acc: {:.4f} F1: {:.4f}'.format(
+
+                print('{} Loss: {:.4f} Acc: {:.4f} F1: {:.4f}'.format(
                     phase, epoch_loss, epoch_acc, epoch_f1))
             else:
                 print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                     phase, epoch_loss, epoch_acc))
-	    	# all_results.write(','.join([str(epoch), phase, str(epoch_loss), str(epoch_acc)]) + '\n')
 
-            if phase == 'train' and epoch_f1 > best_train_acc:
-                best_train_acc = epoch_acc
             # deep copy the model
-            if phase == 'val' and epoch_f1 > best_acc:
+            if phase == 'val' and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = model.state_dict()
 
@@ -127,7 +117,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
     # load best model weights
     model.load_state_dict(best_model_wts)
-    return model, best_train_acc, best_acc
+    return model
 
 class AddisDataset(Dataset):
     """Addis dataset."""
@@ -152,7 +142,7 @@ class AddisDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.root_dir, satellite + '_median_addis_multiband_224x224_%d.npy' % (self.indices[idx]))
+        img_name = os.path.join(self.root_dir, 's1_median_addis_multiband_224x224_%d.npy' % (self.indices[idx]))
         image = np.load(img_name)[:, :, :3]
         labels = self.data[idx]
         if self.transform:
@@ -188,14 +178,13 @@ print "Balances: train: %f, test: %f" % (dataset_train.balance, dataset_test.bal
 
 dataloaders_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 dataloaders_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-dataset_size = len(dataset_train)
 
 use_gpu = torch.cuda.is_available()
 
 ######## Train Model
 
 # torch.set_default_tensor_type('torch.cuda.FloatTensor')
-model_ft = models.resnet18(pretrained=True)
+model_ft = models.resnet50(pretrained=True)
 num_ftrs = model_ft.fc.in_features
 if continuous:
     model_ft.fc = nn.Linear(num_ftrs, 1)
@@ -210,27 +199,9 @@ if not continuous:
 if continuous:
     criterion = nn.MSELoss(size_average=True)
 
-# Observe that all parameters are being optimized
-optimizer_ft = optim.SGD(model_ft.parameters(), lr=lr, momentum=momentum)
+optimizer_ft = optim.Adam(model_ft.parameters(), lr=lr)
 
-# Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=4, gamma=0.1)
 
 model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
                        num_epochs=num_epochs)
-
-# for data in dataloders:
-#     # get the inputs
-#     inputs = data['image']
-#     labels = data['labels'].type(torch.LongTensor)
-
-#     # wrap them in Variable
-#     if use_gpu:
-#         inputs = Variable(inputs.cuda())
-#         labels = Variable(labels.cuda())
-#     else:
-#         inputs, labels = Variable(inputs), Variable(labels)
-
-#     # forward
-#     outputs = model_ft(inputs)
-#     _, preds = torch.max(outputs.data, 1)
